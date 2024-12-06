73f0f9881d5336726afcd7b1a21a57a38f4ab8db
enbraining <me@fodo.dev>
2024-12-06 10:52:11 +0900

    updateheader item gap

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -5,14 +5,14 @@ import React from 'react'
 import type { Header as HeaderType } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
-import Link from 'next/link'
 import { SearchIcon } from 'lucide-react'
+import Link from 'next/link'
 
 export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
   const navItems = header?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className="flex gap-16 items-center">
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="link" />
       })}
