9d512c39d440cbd765fa4ec5b39aecb0c9c1d5a7
enbraining <me@fodo.dev>
2024-12-08 14:34:43 +0900

    remove white theme

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -3,7 +3,6 @@ import { getCachedGlobal } from '@/utilities/getGlobals'
 import type { Footer } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
-import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
 
 export async function Footer() {
   const footer: Footer = await getCachedGlobal('footer', 1)()
@@ -11,10 +10,9 @@ export async function Footer() {
   const navItems = footer?.navItems || []
 
   return (
-    <footer className="border-t border-border bg-black dark:bg-card text-white">
+    <footer className="border-t border-border bg-black bg-card text-white">
       <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <ThemeSelector />
           <nav className="flex flex-col md:flex-row gap-4">
             {navItems.map(({ link }, i) => {
               return <CMSLink className="text-white" key={i} {...link} />
